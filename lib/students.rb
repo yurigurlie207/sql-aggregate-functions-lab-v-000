381566a66958a22282a04f4b3363b9b86dd7ad05
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-30 05:43:08 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,10 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  "SELECT min(gpa) as highest_student_gpa FROM students;"
+  "SELECT min(gpa) as lowest_student_gpa FROM students;"
 end
 
 def average_student_gpa
+
 end
 
 def total_tardies_for_all_students
