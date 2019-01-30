f80cf8f2213fdadad514bf9af1d17de2e7ef3ce2
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-30 05:45:10 +0000

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,23 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
+  "SELECT max(gpa) as highest_student_gpa FROM students;"
 end
 
 def lowest_student_gpa
+  "SELECT min(gpa) as lowest_student_gpa FROM students;"
 end
 
 def average_student_gpa
+  "SELECT avg(gpa) as average_student_gpa FROM students;"
+
 end
 
 def total_tardies_for_all_students
+  "SELECT sum(tardies) as total_tardies_for_all_students FROM students;"
+
 end
 
 def average_gpa_for_9th_grade
+  "SELECT avg(gpa) as average_gpa_for_9th_grade FROM students WHERE grade = 9;"
 end
