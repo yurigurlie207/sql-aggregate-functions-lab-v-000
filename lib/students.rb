3583000c3f063e12adc5874c64aa99ab8062214b
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-30 05:43:56 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -14,7 +14,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT sum(tardies) as lowest_student_gpa FROM students;"
+  "SELECT sum(tardies) as total_tardies_for_all_students FROM students;"
 
 end
 
